ce7551f219853820a37d5c95dae8c4bf68a83ede
Ivan Abreu <iabreufilho@gmail.com>
2025-03-16 16:23:17 -0300

    clean repo

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,13 @@ import os
 
 import openai
 import uvicorn
-from api.routers import chat
 from database import engine
 from dotenv import find_dotenv, load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from models import profile
+
+from backend.app.api.routers import test
 
 _ = load_dotenv(find_dotenv())  # read local .env file
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -33,7 +34,7 @@ async def on_startup():
     create_tables()
 
 
-app.include_router(chat.router, prefix="/api")
+app.include_router(test.router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
