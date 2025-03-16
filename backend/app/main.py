dac5e4843a334eb92263fb91971f70edfa780a5b
Ivan Abreu <iabreufilho@gmail.com>
2025-03-16 16:58:53 -0300

    working version

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,13 @@ import os
 
 import openai
 import uvicorn
-from api.routers import test
 from database import engine
 from dotenv import find_dotenv, load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from models import profile
+
+from backend.app.api.routers import route
 
 _ = load_dotenv(find_dotenv())  # read local .env file
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -28,12 +29,17 @@ app.add_middleware(
 )
 
 
+@app.get("/")
+def get_customer_by_name():
+    return {"message": "Hello World"}
+
+
 @app.on_event("startup")
 async def on_startup():
     create_tables()
 
 
-app.include_router(test.router, prefix="/api")
+app.include_router(route.router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
