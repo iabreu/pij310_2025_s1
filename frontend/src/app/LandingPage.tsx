a039a3acedc198dd2a948a65c9e7bf88f46a1238
Ivan Abreu <iabreufilho@gmail.com>
2025-03-16 15:42:18 -0300

    Fix supabase link

--- frontend/src/app/LandingPage.tsx
+++ frontend/src/app/LandingPage.tsx
@@ -17,14 +17,6 @@ export default function LandingPage() {
 
       <main className="container mx-auto flex flex-col lg:flex-row lg:space-x-8 items-center justify-center text-left min-h-[calc(100vh-72px)] overflow-y-auto pb-8 px-4 md:px-8">
         <div className="flex-1 space-y-6 w-full lg:max-w-lg px-4 pt-2">
-          <h2 className="text-xl md:text-2xl lg:text-3xl font-bold leading-tight text-blue-700 dark:text-blue-400">
-            LLM Applications Starter Template
-          </h2>
-          <p className="text-md md:text-lg text-neutral-700 dark:text-neutral-300">
-            This is an open source AI chatbot app template built with Next.js,
-            the Vercel AI SDK, Shadcn, TailwindCSS, TypeScript and Supabase.
-            Backend is built with FastAPI, Langchain and Supabase database.
-          </p>
           <div className="flex flex-col space-y-4 mt-4">
             <Button
               className="md:text-lg py-4 md:py-6 bg-blue-500 hover:bg-blue-600 text-neutral-100 dark:bg-blue-500 hover:dark:bg-blue-400 dark:text-neutral-100"
