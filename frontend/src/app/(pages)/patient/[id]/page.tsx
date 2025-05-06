c912ad691991fdae2388b3ca4c74cc8d781d93c2
Ivan Abreu <iabreufilho@gmail.com>
2025-05-06 10:34:19 -0300

    Fix deploy

--- frontend/src/app/(pages)/patient/[id]/page.tsx
+++ frontend/src/app/(pages)/patient/[id]/page.tsx
@@ -1,55 +1,54 @@
-'use client'
-import { useEffect, useState } from 'react';
-import { useParams, useRouter } from 'next/navigation';
-import { PatientDataProps, patientService } from '@/services/api'
-import { Modal, Spinner } from '@/components';
-import PageLayout from '@/components/PageLayout';
-import { number } from 'zod';
-import { statusColor } from '@/utils/functions';
-import { Button } from '@/components/ui/button';
-import ExameControl from './ControlExams';
-import { Edit, PlusSquare } from 'lucide-react';
-
+"use client";
+import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { PatientDataProps, patientService } from "@/services/api";
+import { Modal, Spinner } from "@/components";
+import PageLayout from "@/components/PageLayout";
+import { number } from "zod";
+import { statusColor } from "@/utils/functions";
+import { Button } from "@/components/ui/button";
+import ExameControl from "./ControlExams";
+import { Edit, PlusSquare } from "lucide-react";
 
 const PatientPage = () => {
-  const [patientData, setPatientData] = useState<PatientDataProps | null>(null)
-  const [handleOpenNewExam, setHandleOpenNewExam] = useState<boolean>(false)
-  const [isPatientLoading, setIsPatientiLoading] = useState<boolean>(false)
-  const [handleOpenEdit, setHandleOpenEdit] = useState<boolean>(false)
+  const [patientData, setPatientData] = useState<PatientDataProps | null>(null);
+  const [handleOpenNewExam, setHandleOpenNewExam] = useState<boolean>(false);
+  const [isPatientLoading, setIsPatientiLoading] = useState<boolean>(false);
+  const [handleOpenEdit, setHandleOpenEdit] = useState<boolean>(false);
   const params = useParams();
-  const route = useRouter()
+  const route = useRouter();
   const id = params.id;
 
   useEffect(() => {
     if (id) {
-      setIsPatientiLoading(true)
+      setIsPatientiLoading(true);
       try {
-        patientService.getPatient(Number(id))
-          .then((res) => setPatientData(res))
+        patientService
+          .getPatient(Number(id))
+          .then((res) => setPatientData(res));
       } catch (err) {
-        console.error(err)
+        console.error(err);
       } finally {
-        setIsPatientiLoading(false)
+        setIsPatientiLoading(false);
       }
     }
-  }, [id])
+  }, [id]);
 
   const handleBackHome = () => {
-    route.push('/home')
-  }
+    route.push("/home");
+  };
 
   const refetch = () => {
-    setIsPatientiLoading(true)
+    setIsPatientiLoading(true);
 
     try {
-      patientService.getPatient(Number(id))
-        .then((res) => setPatientData(res))
+      patientService.getPatient(Number(id)).then((res) => setPatientData(res));
     } catch (err) {
-      console.error(err)
+      console.error(err);
     } finally {
-      setIsPatientiLoading(false)
+      setIsPatientiLoading(false);
     }
-  }
+  };
 
   if (isPatientLoading) {
     return <Spinner />;
@@ -58,32 +57,47 @@ const PatientPage = () => {
   if (!patientData && !isPatientLoading) {
     return (
       <PageLayout>
-        <section className='flex justify-center items-center m-auto w-full h-full'>
+        <section className="flex justify-center items-center m-auto w-full h-full">
           <button onClick={refetch}>Recarregar</button>
         </section>
       </PageLayout>
-    )
+    );
   }
 
   return (
     <PageLayout>
       <section className="w-full h-full">
-        <Button variant={'ghost'} onClick={handleBackHome}>voltar</Button>
-        <div className='flex md:flex-row flex-col w-full h-ull py-4 text-right justify-center md:justify-end gap-4'>
-          <Button onClick={() => setHandleOpenNewExam(true)} className='w-full md:w-auto'><PlusSquare width={16} className='mr-2' />Adicionar exame</Button>
-          <Button onClick={() => setHandleOpenEdit(true)} className='w-full md:w-auto'><Edit width={16} className='mr-2' /> Editar</Button>
+        <Button variant={"ghost"} onClick={handleBackHome}>
+          voltar
+        </Button>
+        <div className="flex md:flex-row flex-col w-full h-ull py-4 text-right justify-center md:justify-end gap-4">
+          <Button
+            onClick={() => setHandleOpenNewExam(true)}
+            className="w-full md:w-auto"
+          >
+            <PlusSquare width={16} className="mr-2" />
+            Adicionar exame
+          </Button>
+          <Button
+            onClick={() => setHandleOpenEdit(true)}
+            className="w-full md:w-auto"
+          >
+            <Edit width={16} className="mr-2" /> Editar
+          </Button>
         </div>
         <section className="grid grid-cols-1 md:grid-cols-2 gap-5 w-full h-auto">
           <div className="w-full flex flex-col">
-            <h1 className='font-bold text-lg p-2'>Acompanhamento individual</h1>
-            <table className='w-fit flex text-left bg-zinc-700 rounded-md p-2'>
+            <h1 className="font-bold text-lg p-2">Acompanhamento individual</h1>
+            <table className="w-fit flex text-left bg-zinc-700 rounded-md p-2">
               <tbody>
-                <tr className='border-b-2'>
-                  <th className='w-[200px] text-left p-2'>Proturario</th>
+                <tr className="border-b-2">
+                  <th className="w-[200px] text-left p-2">Proturario</th>
                   <td>{patientData?.medical_record_number}</td>
                 </tr>
-                <tr className='border-b-2'>
-                  <th className="w-[200px] text-left p-2">Data exame Inicial</th>
+                <tr className="border-b-2">
+                  <th className="w-[200px] text-left p-2">
+                    Data exame Inicial
+                  </th>
                   <td>{patientData?.diagnosis_date}</td>
                 </tr>
                 <tr>
@@ -94,16 +108,24 @@ const PatientPage = () => {
             </table>
           </div>
           <div className="w-full flex items-center gap-4 p-2">
-            <h1 className='font-bold text-lg'>Estatus atual: </h1>
-            <span className={`${statusColor(patientData?.status || 'Unknown')} text-center w-fit h-fit px-2.5 py-0.5 rounded-full`}>{patientData?.status}</span>
+            <h1 className="font-bold text-lg">Estatus atual: </h1>
+            <span
+              className={`${statusColor(
+                patientData?.status || "Unknown"
+              )} text-center w-fit h-fit px-2.5 py-0.5 rounded-full`}
+            >
+              {patientData?.status}
+            </span>
           </div>
           <div className="w-full flex flex-col justify-center">
-            <h1 className='font-bold text-lg p-2'>Tratamento</h1>
+            <h1 className="font-bold text-lg p-2">Tratamento</h1>
             <table>
               <tbody>
                 <tr>
                   {patientData?.case_histories.map((historie, key: number) => (
-                    <td key={key} className='p-2'>{historie.diagnosis_date}</td>
+                    <td key={key} className="p-2">
+                      {historie.diagnosis_date}
+                    </td>
                   ))}
                 </tr>
               </tbody>
@@ -111,10 +133,22 @@ const PatientPage = () => {
           </div>
           <ExameControl patientData={patientData} />
         </section>
-      </section >
-      {handleOpenNewExam && <Modal.NewExam onOpenChange={setHandleOpenNewExam} open={handleOpenNewExam} id={id} />}
-      {handleOpenEdit && <Modal.EditPatientData onOpenChange={setHandleOpenEdit} open={handleOpenEdit} />}
-    </PageLayout >
+      </section>
+      {handleOpenNewExam && (
+        <Modal.NewExam
+          onOpenChange={setHandleOpenNewExam}
+          open={handleOpenNewExam}
+          // @ts-ignore
+          id={id}
+        />
+      )}
+      {handleOpenEdit && (
+        <Modal.EditPatientData
+          onOpenChange={setHandleOpenEdit}
+          open={handleOpenEdit}
+        />
+      )}
+    </PageLayout>
   );
-}
-export default PatientPage
+};
+export default PatientPage;
